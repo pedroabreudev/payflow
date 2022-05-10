6b05bcde31da480c4a41f781724de3462d6df330
Pedro Henrique <pedroabreu.dev@gmail.com>
2022-05-09 23:08:48 -0400

    update

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,8 @@ void main() {
 }
 
 class AppWidget extends StatelessWidget {
+  const AppWidget({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
